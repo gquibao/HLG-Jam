7a3dafb8e1fce437a12eed612d2502ea7728d6f2
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2019-07-26 00:00:25 -0300

    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Jogar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void Config()
+    {
+        SceneManager.LoadScene("Config");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("SAIU");
+        Application.Quit();
+    }
+}
+
