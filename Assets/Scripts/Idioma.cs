7a764656fdbc8cbec648b1349e6bbe9d7893bc73
Gabriel Santini Quibao <49206000+gquibao@users.noreply.github.com>
2019-07-27 18:00:30 -0300

    Atualizacoes na cena do Jogo

---
+++ Assets/Scripts/Idioma.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idioma
+{
+    [Header("Dialogos")]
+    public static string fala1;
+    public static string fala2;
+    public static string fala3;
+    public static string fala4;
+
+    [Header("Botoes Menu")]
+    public static string btIniciar;
+    public static string btSair;
+    public static string btCreditos;
+    public static string btConfig;
+
+    [Header("Icones Finais")]
+    public static string iconeAprovado;
+    public static string iconeDemitido;
+}
