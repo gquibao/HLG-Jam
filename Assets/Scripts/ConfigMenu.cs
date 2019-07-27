b8fe012deecd76eb81e2831edf92436c25379f16
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2019-07-27 18:11:52 -0300

    Teste de tela
    Teste nas telas de config e creditos

---
+++ Assets/Scripts/ConfigMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigMenu : MonoBehaviour
+{
+    public GameObject _panelConfig;
+
+    public void ExibirMenu(bool _show)
+    {
+        _panelConfig.SetActive(_show);
+    }
+   
+}
