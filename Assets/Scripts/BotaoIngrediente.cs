8a959ff2af8e24fbc472c546eb9e88c1d0ea6bc9
Gabriel Santini Quibao <49206000+gquibao@users.noreply.github.com>
2019-12-04 23:37:49 -0300

    Arduino controller added

--- Assets/Scripts/BotaoIngrediente.cs
+++ Assets/Scripts/BotaoIngrediente.cs
@@ -1,13 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
 
 public class BotaoIngrediente : MonoBehaviour
 {
     public int id;
+    public string ingredientName;
     public GameObject prefab;
 
+    SerialPort sp = new SerialPort("COM3", 9600);
+
+    private void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    private void Update()
+    {
+        try
+        {
+            print(sp.ReadLine());
+            Debug.Log(ingredientName == sp.ReadLine());
+            if (ingredientName  == sp.ReadLine())
+            {
+                CreateIngredients();
+            }
+        }
+        catch (System.Exception)
+        {
+
+        }
+    }
+
     private void OnMouseDown()
+    {
+        CreateIngredients();
+    }
+
+    private void CreateIngredients()
     {
         if (GameManager.instance.ordemIngredientes.Count < 4 && GameManager.instance.isJogoOn)
         {
